1b7f3bfda145599db2298c86f2e3185ba5513e12
Lucas Mogari <lucas@mogari.com.br>
2022-01-12 23:57:01 -0300

    Update config

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,12 +1,5 @@
-const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    return config;
-  },
+  /* config options here */
 };
 
-module.exports = withPlugins([[withBundleAnalyzer({})], nextConfig]);
+module.exports = nextConfig;
