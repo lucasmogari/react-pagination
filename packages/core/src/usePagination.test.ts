72586569e66a955a712764e4514b85330c6cff02
Lucas Mogari <lucas@mogari.com.br>
2024-03-01 11:00:23 -0300

    Fix import

--- packages/core/src/usePagination.test.ts
+++ packages/core/src/usePagination.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import { usePagination } from './usePagination';
 
